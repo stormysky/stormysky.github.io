770ecf5ea451c561af5de6c85f5efde02bf35979
Qingyu <stormysky@users.noreply.github.com>
2018-02-06 14:35:30 +0800

    nav fix

--- assets/js/plugins/nav.js
+++ assets/js/plugins/nav.js
@@ -1,8 +1,35 @@
 (function(){
 	var o = false,du=false;
+	var t=0,l=0;
 	function sh(){
-		var l = $(".list-nav")
-		o = o ? (l.css("opacity","0"),false) : (l.css("opacity","0.8"),true);
+		var s = $(".list-nav");
+		o = o ? (s.css("opacity","0"),false) : (s.css("opacity","0.8"),true);
 	}
+	function down(e){
+		l = e.offsetX;
+		t = e.offsetY;
+		$(window).bind("selectstart",function(){return false;})
+		$(window).bind("mouseover",mv);
+		$(window).bind("mouseup",function(){
+			$(window).unbind("mouseover");
+			$(window).unbind("selectstart");
+			$(window).unbind("mouseup");
+		});
+	}
+	function mv(e){
+		e.preventDefault();
+		e.stopPropagation();
+		var width = $(".list-nav").width();
+		var x = e.clientX - l - width;
+		var y = e.clientY - t;
+		$(".nav").css({"left":x,"top":y});
+	}
+
 	$(".bn-nav").bind("click",sh);
+	$(".bn-nav").bind("mousedown",down);
+
+
+
+
+
 }());
